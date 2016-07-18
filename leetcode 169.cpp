5548545511d67f35ad6693a6bec22f9a26e7a7e2
sailll choi <sailll@sina.cn>
2016-07-18 21:39:06 +0800

    Create leetcode 169.cpp

---
+++ leetcode 169.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element=0;
+        int count=0;
+        for(int i=0;i<nums.size();++i)
+        {
+            if(count==0){
+                element=nums[i];
+                count=1;
+            }
+            else{
+                if(nums[i]!=element){
+                    --count;
+                }
+                else{
+                    ++count;
+                }
+            }
+            
+        }
+        return element;
+    }
+};
