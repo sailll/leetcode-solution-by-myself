d1ace59c0a782a5803ae223ce7e3456299fafa00
sailll choi <sailll@sina.cn>
2016-07-18 21:40:35 +0800

    Update leetcode 169.cpp
    moore voting algorithm

--- leetcode 169.cpp
+++ leetcode 169.cpp
@@ -3,8 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         int element=0;
         int count=0;
-        for(int i=0;i<nums.size();++i)
-        {
+        for(int i=0;i<nums.size();++i){
             if(count==0){
                 element=nums[i];
                 count=1;
