25efca521690a7f2d67285a8b523064736b5c263
sailll choi <sailll@sina.cn>
2016-07-23 17:10:04 +0800

    leetcode 278
    binary search

---
+++ leetcode 278.cpp
@@ -0,0 +1,16 @@
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n){
+        int l=1;
+        int r=n;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(isBadVersion(mid)) r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
+
+};
