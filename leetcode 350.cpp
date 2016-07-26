299bcaf3229b244adf9ff5ef709821e1a7e9a5c6
sailll choi <sailll@sina.cn>
2016-07-26 11:57:38 +0800

    leetcode 350
    hash table ,sort

---
+++ leetcode 350.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int l1=nums1.size();
+        int l2=nums2.size();
+        int a1[10000]={0};
+        int a2[10000]={0};
+        for(int i=0;i<l1;++i){
+            ++a1[nums1[i]];
+        }
+        for(int i=0;i<l2;++i){
+            ++a2[nums2[i]];
+        }
+        for(int i=0;i<10000;++i){
+            if(a1[i]&&a2[i]){
+                for(int j=0;j<min(a1[i],a2[i]);++j){
+                    ans.push_back(i);
+                }
+            }
+        }
+        return ans;
+    }
+};
