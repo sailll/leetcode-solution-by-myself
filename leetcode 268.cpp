cae151fb57f09d65262d1bba6c32a99e4e581d33
sailll choi <sailll@sina.cn>
2016-08-08 00:14:09 +0800

    leetcode 268
    easy

---
+++ leetcode 268.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int l=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<=l;++i){
+            if(nums[i]!=i) return i;
+        }
+        return 0;
+        
+    }
+};
