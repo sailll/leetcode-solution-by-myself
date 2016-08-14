6906e4e82eb148c54d64a943b53b11ac6337b883
sailll choi <sailll@sina.cn>
2016-08-14 15:41:50 +0800

    leetcode 43 using python

---
+++ leetcode 43.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        d1=int(num1)
+        d2=int(num2)
+        ans=d1*d2
+        return str(ans)
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
