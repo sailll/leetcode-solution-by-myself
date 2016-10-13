ea161d242f9a00ae635305823ae8a1a795843270
sailll choi <sailll@sina.cn>
2016-10-13 17:38:01 +0800

    leetcode 20

--- leetcode 20.cpp
+++ leetcode 20.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    string dict="'{[";
-    bool IsLeft(char s){
-        for(int i=0;i<3;++i){
-            if(s==dict[i]) return true;
-        }
-        return false;
-    }
     bool isValid(string s) {
+        map<char,char> dict;
+        dict['(']=')';
+        dict['{']='}';
+        dict['[']=']';
+        stack<char> judge;
         int len=s.length();
-        if(len==0) return true;
         for(int i=0;i<len;++i){
-            if(s[i]=='(') s[i]='('-1;
-        }
-        stack<char> myStack;
-        for(int i=0;i<len;++i){
-            if(IsLeft(s[i])) myStack.push(s[i]);
-            else{
-                if(myStack.empty()) return false;
-                else{
-                    if(s[i]-myStack.top()==2) myStack.pop();
-                    else return false;
+            bool flag=false;
+            for(auto it=dict.begin();it!=dict.end();++it){
+                if(s[i]==it->first){
+                    judge.push(s[i]);
+                    flag=true;
                 }
+                
+            }
+            if(!flag){
+                if(judge.empty()) return false;
+                if(dict[judge.top()]==s[i]) judge.pop();
+                else return false;
             }
         }
-        if(myStack.empty()) return true;
+        if(judge.empty()) return true;
         return false;
+        
     }
-    
 };
